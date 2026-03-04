5ad1a7a4203f051f2eaad1ae36383dd5bd4668da
supremojvy25 <yambao.jimmel@gmail.com>
2026-03-05 05:13:53 +0800

    Update code

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TranslationController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('translations', TranslationController::class);
+
     Route::get('translations/search', [TranslationController::class, 'search']);
     Route::get('translations-export', [TranslationController::class, 'export']);
+
+    Route::apiResource('translations', TranslationController::class);
 });
